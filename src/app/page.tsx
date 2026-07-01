7bdd1bf6557a5d465e0ed3111aae1344a3283299
KunjGupta2006 <kunjg536@gmail.com>
2026-07-01 21:42:44 +0530

    typo in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Analy  tics } from "@vercel/analytics/next"
+// import { Analyics } from "@vercel/analytics/next"
 
 export default function HomePage() {
   return (
