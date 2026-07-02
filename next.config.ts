6151e81611163bdeb5509f082437e87be49fcad1
KunjGupta2006 <kunjg536@gmail.com>
2026-07-02 11:33:53 +0530

    fix: exclude generated prisma from git, fix vercel tracing

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["@prisma/client", "@prisma/engines"],
+  outputFileTracingIncludes: {
+    "**/*": ["./src/generated/prisma/**/*"],
+  },
   images: {
     remotePatterns: [
       {
