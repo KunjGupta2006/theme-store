2b521c2a420d8b71068fd9fd40f7769db661bd15
KunjGupta2006 <kunjg536@gmail.com>
2026-07-02 22:41:38 +0530

    fix prisma engine on vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@prisma/client"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
